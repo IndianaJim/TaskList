7bb9ece70362b2cc91fc45e72780f225419035e3
indianajim <jimrunkel@gmail.com>
2018-09-02 19:53:15 -0400

    delete and filter tasks

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ loadEventListeners();
 function loadEventListeners() {
   //add task event
   form.addEventListener('submit', addTask);
+
+  //remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // clear task events
+  clearBtn.addEventListener('click', clearTasks);
+
+  //filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //add Task
@@ -43,4 +52,36 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  //taskList.innerHTML = '';
+  // or faster with remove child
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
